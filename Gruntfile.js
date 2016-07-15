90fdbc3b8396570739e93e8d22a4015627b6c4ca
Eric Durand-Tremblay <etremblay@kronostechnologies.com>
2016-07-15 09:24:33 -0400

    chore: Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+// vim: set ts=2 sw=2 sts=2 et :
+
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    release: {
+      options: {
+      }
+    }
+  });
+  require('load-grunt-tasks')(grunt);
+
+};
+
